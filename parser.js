4c3a08e833df50246ce6fd7a233b960b2a7e7dc3
Fredrik V. Mørken <187115+fredva@users.noreply.github.com>
2020-11-07 16:41:47 +0100

    stil

--- parser.js
+++ parser.js
@@ -1,18 +1,18 @@
 function toSeconds(timestamp) {
-    var numbers = timestamp.split(".")
-    var mins = Number(numbers[0])
-    var secs = Number(numbers[1])
-    var milis = Number(numbers[2])
-    return mins* 60 + secs + milis/1000
+  var numbers = timestamp.split(".")
+  var mins = Number(numbers[0])
+  var secs = Number(numbers[1])
+  var milis = Number(numbers[2])
+  return mins * 60 + secs + milis / 1000
 }
 
 function parseLyrics(src) {
-    var lines = src//.split("\n")
-    return lines.map(function(line) {
-        var words = line.split(";")
-        var start = toSeconds(words[0])
-        var end = toSeconds(words[1])
-        var text = words[2]
-        return {start, end, text}
-    })
+  var lines = src//.split("\n")
+  return lines.map(function (line) {
+    var words = line.split(";")
+    var start = toSeconds(words[0])
+    var end = toSeconds(words[1])
+    var text = words[2]
+    return {start, end, text}
+  })
 }
--- singalong.js
+++ singalong.js
@@ -12,92 +12,95 @@ var lyrics = fasaden
 console.log(lyrics)
 
 function onYouTubeIframeAPIReady() {
-    console.log("API ready")
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: '4OFRFVnuBEQ',
-        playerVars: {
-            modestbranding: true,
-            autoplay: 1,
-            // controls: 0,
-            showinfo: 0,
-            fs: 0,
-            rel: 0,
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
+  console.log("API ready")
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: '4OFRFVnuBEQ',
+    playerVars: {
+      modestbranding: true,
+      autoplay: 1,
+      // controls: 0,
+      showinfo: 0,
+      fs: 0,
+      rel: 0,
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
 }
 
 function updateLine(next) {
-    currentLine = next.text
-    const textLine = document.getElementById("text")
-    const progressLine = document.getElementById("progress")
-    textLine.innerHTML = next.text
-    progressLine.innerHTML = next.text
-    var duration = next.end - next.start
+  currentLine = next.text
+  const textLine = document.getElementById("text")
+  const progressLine = document.getElementById("progress")
+  textLine.innerHTML = next.text
+  progressLine.innerHTML = next.text
+  var duration = next.end - next.start
 
-    progressLine.setAttribute("style", "");
-    setTimeout(function() {
-        progressLine.setAttribute("style", "animation-duration: " + duration + "s; animation-name: run-text;");
-    }, 10)
+  progressLine.setAttribute("style", "");
+  setTimeout(function () {
+    progressLine.setAttribute("style", "animation-duration: " + duration + "s; animation-name: run-text;");
+  }, 10)
 
 }
 
 function update(time) {
-    if (debug) {
-        document.getElementById("time").innerHTML = time;
-    }
-    var pastLines = lyrics.filter(function(l) {return l.start < time});
-    var next = pastLines[pastLines.length - 1];
-    debug && console.log("next: ", time, next)
-    if (currentLine != next.text) {
-        updateLine(next)
-    }
+  if (debug) {
+    document.getElementById("time").innerHTML = time;
+  }
+  var pastLines = lyrics.filter(function (l) {
+    return l.start < time
+  });
+  var next = pastLines[pastLines.length - 1];
+  debug && console.log("next: ", time, next)
+  if (currentLine != next.text) {
+    updateLine(next)
+  }
 }
 
-function onPlayerReady(event) { }
+function onPlayerReady(event) {
+}
 
 function updateTime() {
-    if (player && player.getCurrentTime) {
-        update(player.getCurrentTime())
-    }
+  if (player && player.getCurrentTime) {
+    update(player.getCurrentTime())
+  }
 }
 
 var intervalId
+
 function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING) {
-        intervalId = setInterval(updateTime, 100);
-    } else {
-        clearInterval(intervalId)
-    }
+  if (event.data == YT.PlayerState.PLAYING) {
+    intervalId = setInterval(updateTime, 100);
+  } else {
+    clearInterval(intervalId)
+  }
 }
 
 function stopVideo() {
-    player.stopVideo();
+  player.stopVideo();
 }
 
 
-
 function setTheme(i) {
-    var themes = ["top", "bottom", "mid"]
-    document.body.className = themes[i % 3]
+  var themes = ["top", "bottom", "mid"]
+  document.body.className = themes[i % 3]
 }
 
 var theme = 0
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    setTheme(theme)
+document.addEventListener("DOMContentLoaded", function (event) {
+  setTheme(theme)
 });
 
 document.addEventListener("keydown", event => {
-    if (event.code === "KeyT") {
-        setTheme(++theme)
-    }
-    if (event.code === "Space") {
-        console.log(player.getCurrentTime())
-    }
+  if (event.code === "KeyT") {
+    setTheme(++theme)
+  }
+  if (event.code === "Space") {
+    console.log(player.getCurrentTime())
+  }
 })
